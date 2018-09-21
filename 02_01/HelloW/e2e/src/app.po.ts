bc38dff2051378e7d20ec33ac8a661d876fa8776
Valerio Sibilla <vsibilla@gmail.com>
2018-09-21 18:15:10 +0200

    created ng HelloW project

--- 02_01/HelloW/e2e/src/app.po.ts
+++ 02_01/HelloW/e2e/src/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class HelloWPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
